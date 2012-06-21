ded7dd054eef016b03e7332945e9ad1567b1496f
nike-17 <nike-17@ya.ru>
2012-06-21 16:50:04 +0700

    fixes

--- tcp-cluster.js
+++ tcp-cluster.js
@@ -1,10 +1,8 @@
 var net = require('net');
-
 var HOST = '127.0.0.1';
 var PORT = 6969;
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
-
 
 if (cluster.isMaster) {
 	// Fork workers.
@@ -14,22 +12,13 @@ if (cluster.isMaster) {
 
 	cluster.on('death', function(worker) {
 		console.log('worker ' + worker.pid + ' died');
+		cluster.fork();
 	});
 } else {
-
-	var server = net.createServer();
-	server.listen(PORT, HOST);
-
-	console.log('Server listening on ' + HOST +':'+ PORT);
-
-	server.on('connection', function(sock) {
-		console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-
-	});
-
-	server.on('data', function(data) {
-		console.log('DATA ' + sock.remoteAddress + ': ' + data);
-		sock.write(data);
-	});
+	net.createServer(function(socket){
+		socket.on('data', function(data) {
+			socket.write(data);
+		});
+	}).listen(PORT, HOST);
 }
 
